ea24cdab32f0c0ef596c6ae28d0d0084d3de1098
Lukas-Hoyer <lukas@hoyer.top>
2023-12-02 16:57:52 +0100

    added new modules and json converter

---
+++ frontend/leocode-frontend/src/app/model/stats.ts
@@ -0,0 +1,11 @@
+
+export interface Stats {
+    suites: number;
+    tests: number;
+    passes: number;
+    pending: number;
+    failures: number;
+    start: string;
+    end: string;
+    duration: number;
+}
--- frontend/leocode-frontend/src/app/test-result/test-result.component.ts
+++ frontend/leocode-frontend/src/app/test-result/test-result.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Result } from '../model/result';
+import { OriginalResult } from '../model/original-result';
 
 @Component({
   selector: 'app-test-result',
@@ -6,13 +8,121 @@ import { Component } from '@angular/core';
   styleUrls: ['./test-result.component.css']
 })
 export class TestResultComponent {
-  result = {
-    message: 'Test-Projekt',
-    passed: 3,
+
+  timer: number = 0;
+
+  result: Result = {
+    message: "Test results",
+    passed: 0,
     notPassed: 0,
-    total: 3
+    total: 0
   };
 
 
-startTest() {}
+  constructor() { 
+    const originalJson = {
+      "stats": {
+        "suites": 1,
+        "tests": 3,
+        "passes": 3,
+        "pending": 0,
+        "failures": 0,
+        "start": "2023-11-16T08:20:11.947Z",
+        "end": "2023-11-16T08:20:11.952Z",
+        "duration": 5
+      },
+      "tests": [
+        {
+          "title": "should return true for a valid password",
+          "fullTitle": "CheckPassword Function should return true for a valid password",
+          "file": "/usr/src/project/test/passwordChecker.spec.js",
+          "duration": 1,
+          "currentRetry": 0,
+          "speed": "fast",
+          "err": {}
+        },
+        {
+          "title": "should return false for an invalid password (too short)",
+          "fullTitle": "CheckPassword Function should return false for an invalid password (too short)",
+          "file": "/usr/src/project/test/passwordChecker.spec.js",
+          "duration": 0,
+          "currentRetry": 0,
+          "speed": "fast",
+          "err": {}
+        },
+        {
+          "title": "should return false for an invalid password (too long)",
+          "fullTitle": "CheckPassword Function should return false for an invalid password (too long)",
+          "file": "/usr/src/project/test/passwordChecker.spec.js",
+          "duration": 0,
+          "currentRetry": 0,
+          "speed": "fast",
+          "err": {}
+        }
+      ],
+      "pending": [],
+      "failures": [],
+      "passes": [
+        {
+          "title": "should return true for a valid password",
+          "fullTitle": "CheckPassword Function should return true for a valid password",
+          "file": "/usr/src/project/test/passwordChecker.spec.js",
+          "duration": 1,
+          "currentRetry": 0,
+          "speed": "fast",
+          "err": {}
+        },
+        {
+          "title": "should return false for an invalid password (too short)",
+          "fullTitle": "CheckPassword Function should return false for an invalid password (too short)",
+          "file": "/usr/src/project/test/passwordChecker.spec.js",
+          "duration": 0,
+          "currentRetry": 0,
+          "speed": "fast",
+          "err": {}
+        },
+        {
+          "title": "should return false for an invalid password (too long)",
+          "fullTitle": "CheckPassword Function should return false for an invalid password (too long)",
+          "file": "/usr/src/project/test/passwordChecker.spec.js",
+          "duration": 0,
+          "currentRetry": 0,
+          "speed": "fast",
+          "err": {}
+        }
+      ]
+    };
+    const originalResult: OriginalResult = originalJson as OriginalResult;
+    this.result = this.convertFromJson(originalResult);
+  }
+
+
+  convertFromJson(originalResult: OriginalResult): Result {
+    const passed : number = originalResult.stats.passes;
+    const notPassed : number = originalResult.stats.failures;
+    const total : number = originalResult.stats.tests;
+
+    const result: Result = {
+        message: "Test results",
+        passed,
+        notPassed,
+        total
+    };
+
+    return result;
+  }
+
+
+  startTest() {
+    // TODO : Reques befehl zu server/backend schicken und messen wie lange es dauert bis die antwort kommt
+    this.timer = 0;
+    const intervalId = setInterval(() => {
+      this.timer++;
+    }, 1000);
+
+    // TODO : Reques befehl zu server/schicken und das json file welches man zurück bekommt convertieren und in result speichern
+
+    clearInterval(intervalId);
+
+  }
 }
