9e2c33d86a07e3715497d682f5faea20c9dcf096
David-Proeller <86597963+David-Proeller@users.noreply.github.com>
2023-12-29 18:21:59 +0100

    ResultFileHelperCSharp

--- backend/WebApplication/Program.cs
+++ backend/WebApplication/Program.cs
@@ -4,147 +4,153 @@ using System.IO;
 using System.Linq;
 using System.Text.Json;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 
-class Program
+namespace LeoCodeBackend
 {
-    private static Process backendProcess;
-
-    static void Main(string[] args)
+    class Program
     {
-        var builder = WebApplication.CreateBuilder(args);
+        private static Process backendProcess;
 
-        builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGen();
-
-        builder.Services.AddCors(options =>
+        static void Main(string[] args)
         {
-            options.AddDefaultPolicy(builder =>
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            builder.Services.AddCors(options =>
             {
-                builder.WithOrigins("http://localhost:4200/test-results", "http://localhost:4200")
-                       .AllowAnyHeader()
-                       .AllowAnyMethod()
-                       .AllowAnyOrigin();
+                options.AddDefaultPolicy(builder =>
+                {
+                    builder.WithOrigins("http://localhost:4200/test-results", "http://localhost:4200")
+                           .AllowAnyHeader()
+                           .AllowAnyMethod()
+                           .AllowAnyOrigin();
+                });
             });
-        });
-        builder.Services.AddSignalR();
+            builder.Services.AddSignalR();
 
-        var app = builder.Build();
-        
-        if (app.Environment.IsDevelopment())
-        {
-            app.UseSwagger();
-            app.UseSwaggerUI();
+            var app = builder.Build();
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseCors("AllowAngularFrontend");
+
+            app.UseHttpsRedirection();
+
+            app.MapPost("/runtests", RunTestsApi)
+                .WithName("RunTestsApi")
+                .WithOpenApi();
+
+            app.MapPost("/start", StartBackend)
+                .WithName("Start")
+                .WithOpenApi();
+
+            app.MapPost("/stop", StopBackend)
+                .WithName("Stop")
+                .WithOpenApi();
+
+            app.Run();
         }
 
-        app.UseCors("AllowAngularFrontend");
-
-        app.UseHttpsRedirection();
-
-        app.MapPost("/runtests", RunTestsApi)
-            .WithName("RunTestsApi")
-            .WithOpenApi();
-
-        app.MapPost("/start", StartBackend)
-            .WithName("Start")
-            .WithOpenApi();
-
-        app.MapPost("/stop", StopBackend)
-            .WithName("Stop")
-            .WithOpenApi();
-
-        app.Run();
-    }
-
-    static async Task<IActionResult> RunTestsApi(string language, string ProgramName)
-    {
-        try
+        static async Task<IActionResult> RunTestsApi(string language, string ProgramName)
         {
-            var cwd = Directory.GetCurrentDirectory();
-
-            var path = $@"{cwd}\..\languages";
-
-            cwd = $@"{path}\{language}\{ProgramName}";
-
-            var command = $"run --rm -v {path}:/usr/src/project -w /usr/src/project pwdchec {language} {ProgramName}";
-            var processInfo = new ProcessStartInfo("docker", command)
+            try
             {
-                CreateNoWindow = true,
-                UseShellExecute = false,
-                RedirectStandardOutput = true,
-                RedirectStandardError = true
-            };
+                var cwd = Directory.GetCurrentDirectory();
 
-            using (var proc = new Process { StartInfo = processInfo, EnableRaisingEvents = true })
-            {
-                proc.Start();
-                await proc.WaitForExitAsync();
+                var path = $@"{cwd}\..\languages";
 
-                var code = proc.ExitCode;
+                cwd = $@"{path}\{language}\{ProgramName}";
 
-                var resultsFile = Directory.GetFiles($"{cwd}\\results", "*.trx").FirstOrDefault();
-
-                if (resultsFile != null)
+                var command = $"run --rm -v {path}:/usr/src/project -w /usr/src/project pwdchec {language} {ProgramName}";
+                var processInfo = new ProcessStartInfo("docker", command)
                 {
-                    string jsonString = await File.ReadAllTextAsync(resultsFile);
+                    CreateNoWindow = true,
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true
+                };
 
-                    var jsonDocument = JsonDocument.Parse(jsonString);
-                    var rootElement = jsonDocument.RootElement;
-
-                    var responseObject = new { data = rootElement };
-
-                    return new OkObjectResult(responseObject);
-                }
-                else
+                using (var proc = new Process { StartInfo = processInfo, EnableRaisingEvents = true })
                 {
-                    var errorObject = new { error = "No results file found." };
-                    return new BadRequestObjectResult(errorObject);
+                    proc.Start();
+                    await proc.WaitForExitAsync();
+
+                    var code = proc.ExitCode;
+
+                    var resultsFile = Directory.GetFiles($"{cwd}\\results", "*.trx").FirstOrDefault();
+
+                    if (resultsFile != null)
+                    {
+                        string jsonString = await File.ReadAllTextAsync(resultsFile);
+
+                        var jsonDocument = JsonDocument.Parse(jsonString);
+                        var rootElement = jsonDocument.RootElement;
+
+                        var responseObject = new { data = rootElement };
+
+                        return new OkObjectResult(responseObject);
+                    }
+                    else
+                    {
+                        var errorObject = new { error = "No results file found." };
+                        return new BadRequestObjectResult(errorObject);
+                    }
                 }
             }
-        }
-        catch (Exception ex)
-        {
-            var errorObject = new { error = $"An error occurred: {ex.Message}" };
-            return new BadRequestObjectResult(errorObject);
-        }
-    }
-
-    static void StartBackend(HttpContext context)
-    {
-        try
-        {
-            string webApiProjectPath = @"../LeoCodeBackend";
-
-            ProcessStartInfo psi = new ProcessStartInfo
+            catch (Exception ex)
             {
-                FileName = "dotnet",
-                Arguments = "run",
-                WorkingDirectory = webApiProjectPath,
-            };
-
-            backendProcess = Process.Start(psi);
-
-            Console.WriteLine("Web API started successfully.");
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine($"Error starting Web API: {ex.Message}");
-        }
-    }
-
-    static void StopBackend(HttpContext context)
-    {
-        try
-        {
-            if (backendProcess != null && !backendProcess.HasExited)
-            {
-                backendProcess.Kill();
-                backendProcess.WaitForExit(); // Optionally wait for the process to exit
-                Console.WriteLine("Web API process killed.");
+                var errorObject = new { error = $"An error occurred: {ex.Message}" };
+                return new BadRequestObjectResult(errorObject);
             }
         }
-        catch (Exception ex)
+
+        static void StartBackend(HttpContext context)
         {
-            Console.WriteLine($"Error stopping Web API: {ex.Message}");
+            try
+            {
+                string webApiProjectPath = @"../LeoCodeBackend";
+
+                ProcessStartInfo psi = new ProcessStartInfo
+                {
+                    FileName = "dotnet",
+                    Arguments = "run",
+                    WorkingDirectory = webApiProjectPath,
+                };
+
+                backendProcess = Process.Start(psi);
+
+                Console.WriteLine("Web API started successfully.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error starting Web API: {ex.Message}");
+            }
+        }
+
+        static void StopBackend(HttpContext context)
+        {
+            try
+            {
+                if (backendProcess != null && !backendProcess.HasExited)
+                {
+                    backendProcess.Kill();
+                    backendProcess.WaitForExit(); // Optionally wait for the process to exit
+                    Console.WriteLine("Web API process killed.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error stopping Web API: {ex.Message}");
+            }
         }
     }
 }
---
+++ backend/WebApplication/ResultFileHelperCSharp.cs
@@ -0,0 +1,103 @@
+﻿using Newtonsoft.Json;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace LeoCodeBackend
+{
+    public class ResultFileHelperCSharp
+    {
+        public void ConvertTrxToJson()
+        {
+            string trxFilePath = "C:\\Schule\\4AHIF\\LeoCode\\backend\\languages\\CSharp\\PasswordTest\\results\\_0d32bfe019ae_2023-12-03_19_07_36.trx";
+            string jsonFilePath = "C:\\test.json";
+
+            try
+            {
+                var testResults = LoadTestResults(trxFilePath);
+
+                Console.WriteLine($"Total Tests: {testResults.Count}");
+                Console.WriteLine($"Passed Tests: {testResults.Count(result => result.Outcome == "Passed")}");
+                Console.WriteLine($"Failed Tests: {testResults.Count(result => result.Outcome == "Failed")}");
+
+                string jsonResult = ConvertTestResultsToJson(testResults);
+
+                File.WriteAllText(jsonFilePath, jsonResult);
+
+                Console.WriteLine("Conversion completed successfully.");
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine($"Error: TRX file not found. {ex.Message}");
+            }
+            catch (XmlException ex)
+            {
+                Console.WriteLine($"Error: XML parsing issue. {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+        }
+        static List<TestResult> LoadTestResults(string trxFilePath)
+        {
+            var testResults = new List<TestResult>();
+
+            XDocument trxDocument = XDocument.Load(trxFilePath);
+
+            XNamespace ns = "http://microsoft.com/schemas/VisualStudio/TeamTest/2010";
+
+            foreach (var testResultElement in trxDocument.Descendants(ns + "UnitTestResult"))
+            {
+                var testResult = new TestResult
+                {
+                    TestName = testResultElement.Attribute("testName").Value,
+                    Outcome = testResultElement.Attribute("outcome").Value,
+                    ErrorMessage = testResultElement.Attribute("errorMessage")?.Value,
+                    ErrorStackTrace = testResultElement.Attribute("errorStackTrace")?.Value,
+                    StartTime = DateTime.ParseExact(testResultElement.Attribute("startTime").Value, "yyyy-MM-ddTHH:mm:ss.FFFFFFFK", null),
+                    EndTime = DateTime.ParseExact(testResultElement.Attribute("endTime").Value, "yyyy-MM-ddTHH:mm:ss.FFFFFFFK", null)
+                };
+
+                testResults.Add(testResult);
+            }
+
+            return testResults;
+        }
+
+        static string ConvertTestResultsToJson(List<TestResult> testResults)
+        {
+            var summary = new
+            {
+                TotalTests = testResults.Count,
+                PassedTests = testResults.Count(result => result.Outcome == "Passed"),
+                FailedTests = testResults.Count(result => result.Outcome == "Failed")
+            };
+
+            var jsonResults = new
+            {
+                Summary = summary,
+                TestResults = testResults.Select(result =>
+                    new
+                    {
+                        TestName = result.TestName,
+                        Outcome = result.Outcome,
+                        ErrorMessage = result.ErrorMessage,
+                        ErrorStackTrace = result.ErrorStackTrace,
+                        StartTime = result.StartTime,
+                        EndTime = result.EndTime
+                    })
+            };
+
+            return JsonConvert.SerializeObject(jsonResults, Newtonsoft.Json.Formatting.Indented);
+        }
+    }
+    class TestResult
+    {
+        public string TestName { get; set; }
+        public string Outcome { get; set; }
+        public string ErrorMessage { get; set; }
+        public string ErrorStackTrace { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+    }
+}
