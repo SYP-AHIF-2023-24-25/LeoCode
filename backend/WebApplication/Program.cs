b1435839a6c8fe1ab8f14c788809e3d8347cc4eb
David-Proeller <86597963+David-Proeller@users.noreply.github.com>
2023-12-29 15:57:48 +0100

    2 backend

--- backend/WebApplication/Program.cs
+++ backend/WebApplication/Program.cs
@@ -1,93 +1,150 @@
+using System;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text.Json;
 using Microsoft.AspNetCore.Mvc;
 
-var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-builder.Services.AddCors(options =>
+class Program
 {
-    options.AddDefaultPolicy(builder =>
+    private static Process backendProcess;
+
+    static void Main(string[] args)
     {
-        builder.WithOrigins("http://localhost:4200/test-results", "http://localhost:4200")
-               .AllowAnyHeader()
-               .AllowAnyMethod()
-               .AllowAnyOrigin();
-    });
-});
-builder.Services.AddSignalR();
+        var builder = WebApplication.CreateBuilder(args);
 
-var app = builder.Build();
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+        builder.Services.AddCors(options =>
+        {
+            options.AddDefaultPolicy(builder =>
+            {
+                builder.WithOrigins("http://localhost:4200/test-results", "http://localhost:4200")
+                       .AllowAnyHeader()
+                       .AllowAnyMethod()
+                       .AllowAnyOrigin();
+            });
+        });
+        builder.Services.AddSignalR();
 
-app.UseCors("AllowAngularFrontend");
+        var app = builder.Build();
+        
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
 
-app.UseHttpsRedirection();
+        app.UseCors("AllowAngularFrontend");
 
-app.MapPost("/runtests", RunTestsHandler)
-    .WithName("RunTests")
-    .WithOpenApi();
+        app.UseHttpsRedirection();
 
-app.Run();
+        app.MapPost("/runtests", RunTestsApi)
+            .WithName("RunTestsApi")
+            .WithOpenApi();
 
-async Task<IActionResult> RunTestsHandler(string language, string ProgramName)
-{
-    try
+        app.MapPost("/start", StartBackend)
+            .WithName("Start")
+            .WithOpenApi();
+
+        app.MapPost("/stop", StopBackend)
+            .WithName("Stop")
+            .WithOpenApi();
+
+        app.Run();
+    }
+
+    static async Task<IActionResult> RunTestsApi(string language, string ProgramName)
     {
-        var cwd = Directory.GetCurrentDirectory();
-        var parentDirectory = Directory.GetParent(cwd).FullName;
-        var targetDirectory = Path.Combine(parentDirectory, "languages");
-        var path = @"C:\Schule\4AHIF\LeoCode\backend\languages\";
-        cwd = $@"C:\Schule\4AHIF\LeoCode\backend\languages\{language}\{ProgramName}";
-
-        var command = $"run --rm -v {path}:/usr/src/project -w /usr/src/project pwdchecker {language} {ProgramName}";
-        var processInfo = new ProcessStartInfo("docker", command)
+        try
         {
-            CreateNoWindow = true,
-            UseShellExecute = false,
-            RedirectStandardOutput = true,
-            RedirectStandardError = true
-        };
+            var cwd = Directory.GetCurrentDirectory();
 
-        using (var proc = new Process { StartInfo = processInfo, EnableRaisingEvents = true })
-        {
-            proc.Start();
-            await proc.WaitForExitAsync();
+            var path = $@"{cwd}\..\languages";
 
-            var code = proc.ExitCode;
+            cwd = $@"{path}\{language}\{ProgramName}";
 
-            var resultsFile = Directory.GetFiles($"{cwd}\\results", "*.json").FirstOrDefault();
-
-            if (resultsFile != null)
+            var command = $"run --rm -v {path}:/usr/src/project -w /usr/src/project pwdchecker {language} {ProgramName}";
+            var processInfo = new ProcessStartInfo("docker", command)
             {
-                string jsonString = await File.ReadAllTextAsync(resultsFile);
+                CreateNoWindow = true,
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true
+            };
 
-                var jsonDocument = JsonDocument.Parse(jsonString);
-                var rootElement = jsonDocument.RootElement;
-
-                var responseObject = new { data = rootElement };
-
-                return new OkObjectResult(responseObject);
-            }
-            else
+            using (var proc = new Process { StartInfo = processInfo, EnableRaisingEvents = true })
             {
-                var errorObject = new { error = "No results file found." };
-                return new BadRequestObjectResult(errorObject);
+                proc.Start();
+                await proc.WaitForExitAsync();
+
+                var code = proc.ExitCode;
+
+                var resultsFile = Directory.GetFiles($"{cwd}\\results", "*.json").FirstOrDefault();
+
+                if (resultsFile != null)
+                {
+                    string jsonString = await File.ReadAllTextAsync(resultsFile);
+
+                    var jsonDocument = JsonDocument.Parse(jsonString);
+                    var rootElement = jsonDocument.RootElement;
+
+                    var responseObject = new { data = rootElement };
+
+                    return new OkObjectResult(responseObject);
+                }
+                else
+                {
+                    var errorObject = new { error = "No results file found." };
+                    return new BadRequestObjectResult(errorObject);
+                }
             }
         }
+        catch (Exception ex)
+        {
+            var errorObject = new { error = $"An error occurred: {ex.Message}" };
+            return new BadRequestObjectResult(errorObject);
+        }
     }
-    catch (Exception ex)
+
+    static void StartBackend(HttpContext context)
     {
-        var errorObject = new { error = $"An error occurred: {ex.Message}" };
-        return new BadRequestObjectResult(errorObject);
+        try
+        {
+            string webApiProjectPath = @"../LeoCodeBackend";
+
+            ProcessStartInfo psi = new ProcessStartInfo
+            {
+                FileName = "dotnet",
+                Arguments = "run",
+                WorkingDirectory = webApiProjectPath,
+            };
+
+            backendProcess = Process.Start(psi);
+
+            Console.WriteLine("Web API started successfully.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error starting Web API: {ex.Message}");
+        }
+    }
+
+    static void StopBackend(HttpContext context)
+    {
+        try
+        {
+            if (backendProcess != null && !backendProcess.HasExited)
+            {
+                backendProcess.Kill();
+                backendProcess.WaitForExit(); // Optionally wait for the process to exit
+                Console.WriteLine("Web API process killed.");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error stopping Web API: {ex.Message}");
+        }
     }
 }
