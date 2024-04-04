5df1aeb5357c0eb2d4851310f0012c50a5cd9e54
David-Proeller <86597963+David-Proeller@users.noreply.github.com>
2024-04-04 02:27:08 +0200

    Delete backend/languages/Typescript/node_modules directory

--- backend/languages/Typescript/node_modules/passwordchecker/src/passwordChecker.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.CheckPassword = void 0;
-function CheckPassword(password) { return true; }
-exports.CheckPassword = CheckPassword;
