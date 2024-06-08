ae7cae8234085e7494970638f459e0b2283c7556
Florian-Hagmair <flohagi14@gmail.com>
2024-06-08 15:51:09 +0200

    put request mit gleichzeitigen post für den user mit der exercise wenn er sie neu aunfängt

--- backend/db/Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
+++ backend/db/Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6066f1f4fedc9c2788a20fecc21430454fa7d8cf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+089ed8263628f1e2cf797a27292e6140960b2c7b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- backend/db/WebAPI/obj/Debug/net8.0/WebAPI.AssemblyInfo.cs
+++ backend/db/WebAPI/obj/Debug/net8.0/WebAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6066f1f4fedc9c2788a20fecc21430454fa7d8cf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+089ed8263628f1e2cf797a27292e6140960b2c7b")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
