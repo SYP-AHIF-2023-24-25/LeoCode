14acf4cde0b428fea3717154a59a3a583559089a
Lukas-Hoyer <lukas@hoyer.top>
2024-06-08 16:06:56 +0200

    fixed bug

--- backend/LeoCodeBackend/obj/Debug/net8.0/LeoCodeBackend.AssemblyInfo.cs
+++ backend/LeoCodeBackend/obj/Debug/net8.0/LeoCodeBackend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LeoCodeBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c84906bf6310ba6698e6fbb8d6a22453744424b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ed6ad5f1986e98a2dd6d2e9f319965b68e2659e")]
 [assembly: System.Reflection.AssemblyProductAttribute("LeoCodeBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LeoCodeBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- backend/db/Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
+++ backend/db/Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44a733e983b355abc62f183d3c96e5dbbdc27dee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ed6ad5f1986e98a2dd6d2e9f319965b68e2659e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
