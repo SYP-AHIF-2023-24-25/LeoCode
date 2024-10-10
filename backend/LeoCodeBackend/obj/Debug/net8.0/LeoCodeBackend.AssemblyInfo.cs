526e1ae96a66544e0668ab3bcfe2c972d68adfd6
David-Proeller <dap030905@gmail.com>
2024-10-10 23:59:19 +0200

    give error message from unit tests to frontend

--- backend/LeoCodeBackend/obj/Debug/net8.0/LeoCodeBackend.AssemblyInfo.cs
+++ backend/LeoCodeBackend/obj/Debug/net8.0/LeoCodeBackend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LeoCodeBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c01c9e31653b1c7ee76a1bd78f79ad5a3a7d0d4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+694d7e18fd292dbac2be7336c0d3514f7c262f15")]
 [assembly: System.Reflection.AssemblyProductAttribute("LeoCodeBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LeoCodeBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
