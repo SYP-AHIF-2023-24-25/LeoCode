283c02b3f6d20be27da858954b6ded9f4a76752e
David-Proeller <dap030905@gmail.com>
2025-01-08 23:21:54 +0100

    production

--- frontend/leocode-frontend/src/environments/environment.prod.ts
+++ frontend/leocode-frontend/src/environments/environment.prod.ts
@@ -1,9 +1,8 @@
 export const environment = {
-    production: true,
-    dbUrl: 'https://your-production-api.com/api',
-    apiUrl: 'https://your-production-api.com/',
-    uploadUrl: 'https://your-production-upload.com/',
-    cSharpUrl: 'https://your-production-csharp.com/',
-    kcUrl: 'https://your-production-api-url/api/demo/',
-  };
-  
+  production: true,
+  dbUrl: 'http://LeoCode.htl-leonding.ac.at:8083/api',
+  apiUrl: 'http://LeoCode.htl-leonding.ac.at:8081/',
+  uploadUrl: 'http://LeoCode.htl-leonding.ac.at:8085/',
+  cSharpUrl: 'http://LeoCode.htl-leonding.ac.at:8082/',
+  kcUrl: 'http://LeoCode.htl-leonding.ac.at:8084/api/demo/',
+};
