c6b8710ab133d1c99b6c3799a00e009176d56f9d
Lukas-Hoyer <lukas@hoyer.top>
2025-04-22 22:08:47 +0200

    fixed mergeCodesection for multiple editable sections

--- backend/db/Base/obj/Debug/net8.0/Base.AssemblyInfo.cs
+++ backend/db/Base/obj/Debug/net8.0/Base.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Base")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0dcaa98352421b547453d976795ea2d17308c192")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2ccd411baf216622bda0af1c0a47331f02000b8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Base")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Base")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
